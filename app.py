c83e15d8c3ed4f231909973af6d3f5ca5d523dc3
azjimenezmartinez <azucenaljim1@csu.fullerton.edu>
2025-10-02 17:33:09 -0700

    connect db to flask interface

--- app.py
+++ app.py
@@ -3,36 +3,20 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["cair_db"]
-patients_collection = db["patients"]
+client = MongoClient('localhost',27017)
 
-@app.route("/")
+
+#This is a mongodb database 
+db = client.flask_database 
+#Mongodb database 
+patients = db.patients 
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
-@app.route("/patients")
-def patients():
-    patients = list(patients_collection.find())
-    return render_template("patients.html", patients=patients)
 
-@app.route("/add_patient", methods=["GET", "POST"])
-def add_patient():
-    if request.method == "POST":
-        name = request.form["name"]
-        age = request.form["age"]
-        symptoms = request.form["symptoms"]
-
-        patient = {
-            "name": name,
-            "age": age,
-            "symptoms": symptoms,
-            "recommendation": "will use AI"  
-        }
-        patients_collection.insert_one(patient)
-        return redirect(url_for("patients"))
-    
-    return render_template("add_patient.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
